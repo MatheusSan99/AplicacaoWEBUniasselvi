19710017a8553f0fd9679bfa81ced107f267bd92
MatheusSan <mdsmatheus100@gmail.com>
2024-06-07 23:23:17 -0300

    ajuste de design

--- views/news-list.php
+++ views/news-list.php
@@ -7,56 +7,41 @@ use Seminario\Mvc\Entity\News;
 /** @var News[] $newsList */
 ?>
 
-<div class="content">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="card">
-          <div class="card-header card-header-primary">
-            <h4 class="card-title">Lista de Notícias</h4>
-            <p class="card-category">Aqui estão as notícias mais recentes</p>
-          </div>
-          <div class="card-body">
-            <div class="table-responsive">
-              <table class="table">
-                <thead class=" text-primary">
-                  <th>
-                    Título
-                  </th>
-                  <th>
-                    Conteúdo
-                  </th>
-                  <th>
-                    Autor
-                  </th>
-                  <th>
-                    Data
-                  </th>
-                  <th>
-                    Ações
-                  </th>
-                </thead>
-                <tbody>
-                  <?php foreach ($newsList as $news): ?>
-                    <tr>
-                      <td><?= $news->getTitle(); ?></td>
-                      <td><?= $news->getContent(); ?></td>
-                      <td><?= $news->getAuthor(); ?></td>
-                      <td><?= $news->getDate(); ?></td>
-                      <td>
-                        <a href="/editar-noticia?id=<?= $news->getId(); ?>" class="btn btn-info btn-sm">Editar</a>
-                        <a href="/remover-noticia?id=<?= $news->getId(); ?>" class="btn btn-danger btn-sm">Excluir</a>
-                      </td>
-                    </tr>
-                  <?php endforeach; ?>
-                </tbody>
-              </table>
-            </div>
-          </div>
+<body class="login-page sidebar-collapse">
+  <div class="page-header clear-filter" filter-color="orange">
+    <main class="container mt-5">
+      <div class="card card-body text-black">
+        <h2 class="title text-center">Lista de Notícias</h2>
+        <p class="card-category text-center">Aqui estão as notícias mais recentes</p>
+        <div class="table-responsive">
+          <table class="table">
+            <thead class="text-primary">
+              <tr>
+                <th>Título</th>
+                <th>Conteúdo</th>
+                <th>Autor</th>
+                <th>Data</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php foreach ($newsList as $news): ?>
+                <tr>
+                  <td><?= htmlspecialchars($news->getTitle(), ENT_QUOTES, 'UTF-8'); ?></td>
+                  <td><?= htmlspecialchars($news->getContent(), ENT_QUOTES, 'UTF-8'); ?></td>
+                  <td><?= htmlspecialchars($news->getAuthor(), ENT_QUOTES, 'UTF-8'); ?></td>
+                  <td><?= htmlspecialchars($news->getDate(), ENT_QUOTES, 'UTF-8'); ?></td>
+                  <td>
+                    <a href="/editar-noticia?id=<?= $news->getId(); ?>" class="btn btn-info btn-round btn-sm">Editar</a>
+                    <a href="/remover-noticia?id=<?= $news->getId(); ?>" class="btn btn-danger btn-round btn-sm">Excluir</a>
+                  </td>
+                </tr>
+              <?php endforeach; ?>
+            </tbody>
+          </table>
         </div>
       </div>
-    </div>
+    </main>
   </div>
-</div>
 
 <?php require_once __DIR__ . '/fim-html.php'; ?>
